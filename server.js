750790077fff9553cadab25921dfeea6eeb80c54
Kelsea Fields <kelsea.fields@yahoo.com>
2021-02-25 14:24:26 -0800

    change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var NotesObj = require("./develop/db/db.json");
+// var NotesObj = require("./develop/db/db.json");
 var path = require("path");
 
 var PORT = process.env.PORT || 3001;
